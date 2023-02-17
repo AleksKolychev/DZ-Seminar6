409d0dc4b6c3de9c33e1fd7d1372b5d6e5a2f3e4
AleksKolychev <aleksushome@gmail.com>
2023-02-17 10:15:41 +0500

    Add Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Пользователь вводит М чисел. 
+Посчитайте, сколько чисел больше 0 ввел пользователь.*/
+Console.Clear();
+Console.Write("Введите количество чисел:");
+int m = Convert.ToInt32(Console.ReadLine()), count = 0;
+for (int i = 0; i < m; i++)
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine(count);
